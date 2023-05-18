d03c00b68e04ceb1e59162bedae2ad1da40d2e5a
William Yang <williamyang2017@gmail.com>
2023-05-19 00:31:16 +1000

    Updated build scripts

--- fx.bat
+++ fx.bat
@@ -1,23 +1,27 @@
 @echo off
 
 :GETOPTS
-if /I "%1" == "vsdev" call cmd /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+if /I "%1" == "vsdev" call cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
 if /I "%1" == "cmake-conf" call^
  cmake --no-warn-unused-cli^
  -DCMAKE_TOOLCHAIN_FILE:STRING=C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake^
  -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE^
- -B"build" -G"Visual Studio 16 2019"
+ -B"build" -G"Visual Studio 17 2022"^
+ -A"x64"
 
 if /I "%1" == "build" goto BUILD
 if /I "%1" == "help" goto HELP
-goto EXIT
+goto HELP
 
 :BUILD
-set ARGS=/p:CL_MPCOUNT=4 /m:4 /v:minimal /nologo
+set BUILD_REF="true"
+if "%4" == "--ignore-ref" set BUILD_REF="false" 
+
+set ARGS=/p:CL_MPCOUNT=%NUMBER_OF_PROCESSORS% /m:%NUMBER_OF_PROCESSORS% /p:BuildProjectReferences=%BUILD_REF% /v:minimal /nologo 
 
 set PROJ_PATH="%3"
-if "%~3" == "" set PROJ_PATH="build\main.vcxproj" 
+if "%~3" == "" set PROJ_PATH="build\basic_radio_app.vcxproj" 
 
 if /I "%2" == ""        call msbuild %PROJ_PATH% /p:Configuration=Release          %ARGS%
 if /I "%2" == "release" call msbuild %PROJ_PATH% /p:Configuration=Release          %ARGS%
